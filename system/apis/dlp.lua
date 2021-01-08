50d0e2cb949d6f2dc510e4a7998eda417bbc0f74
TheRealEli310 <33650824+TheRealEli310@users.noreply.github.com>
2021-01-08 16:16:55 +1100

    Update dlp.lua

--- system/apis/dlp.lua
+++ system/apis/dlp.lua
@@ -1,1 +1,17 @@
-function activate()
+function activate(side)
+  return rednet.open(side)
+end
+function dohost(prog)
+  rednet.host("CboxDl",tostring(os.getComputerID()))
+  local s,m,p = rednet.receive()
+  sleep(0.1)
+  rednet.send(s,prog)
+end
+function listdls()
+  return rednet.lookup("CboxDl")
+end
+function dodl(id)
+  rednet.send(id,"")
+  local s,m,p = rednet.receive()
+  return {s,m}
+end
