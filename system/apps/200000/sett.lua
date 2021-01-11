ac6a832678bc4e68f61b2a9331b743ec7fa5c8a5
TheRealEli310 <33650824+TheRealEli310@users.noreply.github.com>
2021-01-11 17:40:05 +1100

    Update sett.lua

--- system/apps/200000/sett.lua
+++ system/apps/200000/sett.lua
@@ -1,7 +1,7 @@
 -- Menu API provided by cyanisaac and ProjectB, licensed under the MIT license.
 title = "Settings"
 while true do
-    sel = menu.doMenu(title,{"Back","Language","System Update"})
+    sel = menu.doMenu(title,{"Back","Language","System Update","Factory Reset"})
     if sel == 1 then
         break
     end
@@ -9,12 +9,15 @@ while true do
         lang = menu.doMenu("Language",{"English"})
         settings.set("sys.language",lang)
     end
-	if sel == 3 then
-		updsel = menu.doMenu("System Update",{"Update via Internet","Install from disk"})
-		if updsel == 1 then
-			shell.dir("")
-			shell.run("gitget TheRealEli310 cbox main")
-			os.reboot()
-		end
+    if sel == 3 then
+	updsel = menu.doMenu("System Update",{"Update via Internet","Install from disk"})
+	if updsel == 1 then
+		shell.dir("")
+		shell.run("gitget TheRealEli310 cbox main")
+		os.reboot()
 	end
+    end
+    if sel == 4 then
+	menu.doInfoScreen("Insert recovery disk and press ENTER.")
+    end
 end
